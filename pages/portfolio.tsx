8cbe2c095b055dc932367233953cd73e92296e46
tamagram <tamagram64@gmail.com>
2021-11-01 18:20:55 +0900

    :sparkles: Apply CSS to `Blog` and `Portfolio` pages.

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -4,22 +4,72 @@ import Image from "next/image";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import styles from "./portfolio.module.css";
 
 const Portfolio: NextPage = () => (
   <Layout>
     <Header path="/portfolio" />
-    <main>
-      <h1>Projects</h1>
+    <main className={styles.main}>
+      <h1 className={styles.main__heading_1}>Projects</h1>
       <ul>
-        <li>
-          <article>
-            title
-            <Image
-              src="/120x120.png"
-              alt="placehold"
-              width={120}
-              height={120}
-            />
+        <li className={styles.main__ul__li}>
+          <article className={styles.main__ul__li__article}>
+            <h2 className={styles.main__ul__li__article__h2}>
+              titlessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+            </h2>
+            <div className={styles.main__ul__li__article__div}>
+              <Image
+                src="/120x120.png"
+                alt="placehold"
+                width={90}
+                height={90}
+              />
+            </div>
+          </article>
+        </li>
+        <li className={styles.main__ul__li}>
+          <article className={styles.main__ul__li__article}>
+            <h2 className={styles.main__ul__li__article__h2}>
+              titlessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+            </h2>
+            <div className={styles.main__ul__li__article__div}>
+              <Image
+                src="/120x120.png"
+                alt="placehold"
+                width={90}
+                height={90}
+              />
+            </div>
+          </article>
+        </li>
+        <li className={styles.main__ul__li}>
+          <article className={styles.main__ul__li__article}>
+            <h2 className={styles.main__ul__li__article__h2}>
+              titlessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+            </h2>
+            <div className={styles.main__ul__li__article__div}>
+              <Image
+                src="/120x120.png"
+                alt="placehold"
+                width={90}
+                height={90}
+              />
+            </div>
+          </article>
+        </li>
+        <li className={styles.main__ul__li}>
+          <article className={styles.main__ul__li__article}>
+            <h2 className={styles.main__ul__li__article__h2}>
+              titlessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+            </h2>
+            <div className={styles.main__ul__li__article__div}>
+              <Image
+                src="/120x120.png"
+                alt="placehold"
+                width={90}
+                height={90}
+              />
+            </div>
           </article>
         </li>
       </ul>
