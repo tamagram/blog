6dd3d75e29c7d3da4b189413573be4bb74519164
tamagram <tamagram64@gmail.com>
2021-11-03 14:20:13 +0900

    :ballot_box_with_check: Pass test.

--- apis/hatena.ts
+++ apis/hatena.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { JSDOM } from "jsdom";
+import POST from "../entities/post";
 require("dotenv").config();
 
 const hatenaName = process.env.HATENA_NAME;
 const hatenaPass = process.env.HATENA_PASS;
 
-const getXmlDocument = async () => {
+export const getXmlDocument = async () => {
   const data = await (async () => {
     return await axios
       .get(
@@ -28,8 +29,24 @@ const getXmlDocument = async () => {
   return xmlData;
 };
 
-const getPosts = (document: Document) => {
-  
-}
-
-export default getXmlDocument;
+export const getPosts = (document: Document) => {
+  const gotEntry = document.getElementsByTagName("entry");
+  const posts = [];
+  for (let i = 0; i < gotEntry.length; i++) {
+    const gotTitle = gotEntry[i].getElementsByTagName("title");
+    const gotContent = gotEntry[i].getElementsByTagName("content");
+    const gotPublished = gotEntry[i].getElementsByTagName("published");
+    const gotUpdated = gotEntry[i].getElementsByTagName("updated");
+    const gotLink = gotEntry[i].getElementsByTagName("link");
+    const post: POST = {
+      title: gotTitle[0].textContent,
+      content: gotContent[0].textContent,
+      published: new Date(gotPublished[0].textContent),
+      updated: new Date(gotUpdated[0].textContent),
+      tags: [],
+      link: gotLink[1].getAttribute("href"),
+    };
+    posts.push(post);
+  }
+  return posts;
+};
