5a21dc88cfbb87ec546c330e57e1552b3c767f91
tamagram <tamagram64@gmail.com>
2021-11-03 15:56:26 +0900

    :ballot_box_with_check: Update test.

--- apis/hatena.ts
+++ apis/hatena.ts
@@ -29,28 +29,14 @@ export const getXmlDocument = async () => {
   return xmlData;
 };
 
-export const getPosts = (document: Document): POST[] => {
+export const getPostLinks = (document: Document): POST[] => {
   const gotEntry = document.getElementsByTagName("entry");
-  const posts = [];
+  const links = [];
   for (let i = 0; i < gotEntry.length; i++) {
-    const gotId = gotEntry[i].getElementsByTagName("id");
-    const gotTitle = gotEntry[i].getElementsByTagName("title");
-    const gotContent = gotEntry[i].getElementsByTagName("content");
-    const gotPublished = gotEntry[i].getElementsByTagName("published");
-    const gotUpdated = gotEntry[i].getElementsByTagName("updated");
     const gotLink = gotEntry[i].getElementsByTagName("link");
-    const post: POST = {
-      id: gotId[0].textContent,
-      title: gotTitle[0].textContent,
-      content: gotContent[0].textContent,
-      published: new Date(gotPublished[0].textContent),
-      updated: new Date(gotUpdated[0].textContent),
-      tags: [],
-      link: gotLink[1].getAttribute("href"),
-    };
-    posts.push(post);
+    links.push(gotLink[0].getAttribute("href"));
   }
-  return posts;
+  return links;
 };
 
 export const getPost = (document: Document): POST => {
