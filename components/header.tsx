a7f462ddb64659e47140f5556e3102b90db4b3cd
tamagram <tamagram64@gmail.com>
2021-11-08 04:59:19 +0900

    :iphone: Work on responsive design.

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./header.module.css";
 
 type ROUTE = {
@@ -52,6 +53,59 @@ const Header: React.FC<ROUTE> = (props) => (
         </Link>
       </li>
     </nav>
+    <div className={styles.header__div_menu}>
+      <a href="#light_box">
+        <Image src="/menu.svg" alt="menu" width={40} height={40} />
+      </a>
+    </div>
+    <div className={styles.header__div_light_box} id="light_box">
+      <div className={styles.header__div_light_box__div_menu_close}>
+        <a href="#">
+          <Image src="/menu-close.svg" alt="menu" width={40} height={40} />
+        </a>
+      </div>
+      <nav className={styles.header__div_light_box__nav}>
+        <li className={styles.header__div_light_box__nav__li}>
+          <Link href="/">
+            <a
+              className={
+                props.path === AboutPath
+                  ? styles.header__div_light_box__nav__li__selected_a
+                  : styles.header__div_light_box__nav__li__a
+              }
+            >
+              About
+            </a>
+          </Link>
+        </li>
+        <li className={styles.header__div_light_box__nav__li}>
+          <Link href="/blog">
+            <a
+              className={
+                props.path === BlogPath
+                  ? styles.header__div_light_box__nav__li__selected_a
+                  : styles.header__div_light_box__nav__li__a
+              }
+            >
+              Blog
+            </a>
+          </Link>
+        </li>
+        <li className={styles.header__div_light_box__nav__li}>
+          <Link href="/portfolio">
+            <a
+              className={
+                props.path === PortfolioPath
+                  ? styles.header__div_light_box__nav__li__selected_a
+                  : styles.header__div_light_box__nav__li__a
+              }
+            >
+              Portfolio
+            </a>
+          </Link>
+        </li>
+      </nav>
+    </div>
   </header>
 );
 
