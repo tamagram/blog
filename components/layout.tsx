5a877b4ad9536993b3e353e86c7443b36d5c4150
tamagram <tamagram64@gmail.com>
2021-11-09 04:55:18 +0900

    :sparkles: Apply favicon.

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,10 +4,12 @@ const Layout: React.FC = ({ children }) => (
   <div className={styles.layout}>
     <Head>
       <title>tama</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicons/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
     {children}
   </div>
 );
+
 
 export default Layout;
