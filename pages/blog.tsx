ea50603ec820fd0a93fa247843e7edb75d08f6bf
tamagram <tamadevelopment211@gmail.com>
2022-04-27 12:28:24 +0900

    :recycle: Refactor code

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,24 +1,23 @@
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import styles from "./blog.module.css";
-import axios from "axios";
-import { JSDOM } from "jsdom";
 import LINK from "../types/link";
+import useSWR from 'swr';
 
-type Props = {
-  links: LINK[];
-};
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
-const hatenaName = process.env.NEXT_PUBLIC_HATENA_NAME;
-const hatenaPass = process.env.NEXT_PUBLIC_HATENA_PASS;
+const Blog: NextPage = () => {
+  const { data, error } = useSWR<LINK[]>(
+    "api/hatena",
+    fetcher
+  );
 
-const Blog: NextPage<Props> = ({ links }) => {
   const linksLi = () =>
-    links.map((link) => (
+    data.map((link) => (
       <li key={link.id} className={styles.main__ul__li}>
         <Link href={link.local}>
           <a>
@@ -37,6 +36,26 @@ const Blog: NextPage<Props> = ({ links }) => {
         </Link>
       </li>
     ));
+  if (error) return (
+    <Layout>
+      <Header path="/blog" />
+      <main className={styles.main}>
+        <h1 className={styles.main__heading_1}>Posts</h1>
+        An error has occurred.
+      </main>
+      <Footer />
+    </Layout>
+  );
+  if (!data) return (
+    <Layout>
+      <Header path="/blog" />
+      <main className={styles.main}>
+        <h1 className={styles.main__heading_1}>Posts</h1>
+        Loading...
+      </main>
+      <Footer />
+    </Layout>
+  );
   return (
     <Layout>
       <Header path="/blog" />
@@ -49,73 +68,5 @@ const Blog: NextPage<Props> = ({ links }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const getXmlData = async (url: string, config: {} = {}) => {
-    const jsdom = new JSDOM();
-    const parser = new jsdom.window.DOMParser();
-    const data = await axios.get(url, config).then((res) => res.data);
-    const xmlData = parser.parseFromString(data, "text/xml");
-    return xmlData;
-  };
-
-  const getHatenaLinks = async () => {
-    const xmlData = await getXmlData(
-      "https://blog.hatena.ne.jp/tamagram/tamagram.hatenablog.com/atom/entry",
-      {
-        auth: {
-          username: hatenaName,
-          password: hatenaPass,
-        },
-      }
-    );
-    const gotEntry = xmlData.getElementsByTagName("entry");
-    const links: LINK[] = [];
-    for (let i = 0; i < gotEntry.length; i++) {
-      const gotId = gotEntry[i].getElementsByTagName("id");
-      const gotTitle = gotEntry[i].getElementsByTagName("title");
-      const gotPublished = gotEntry[i].getElementsByTagName("published");
-      links.push({
-        id: gotId[0].textContent,
-        title: gotTitle[0].textContent,
-        local: "/posts/hatena/" + gotId[0].textContent.split("-").pop(),
-        reference: "hatena",
-        createdAt: new Date(gotPublished[0].textContent),
-      });
-    }
-    return links;
-  };
-
-  const getZennLinks = async () => {
-    const xmlData = await getXmlData("https://zenn.dev/tamagram/feed");
-    const gotItem = xmlData.getElementsByTagName("item");
-    const links: LINK[] = [];
-    for (let i = 0; i < gotItem.length; i++) {
-      const gotId = gotItem[i].getElementsByTagName("guid");
-      const gotTitle = gotItem[i].getElementsByTagName("title");
-      const gotPubDate = gotItem[i].getElementsByTagName("pubDate");
-      links.push({
-        id: gotId[0].textContent.split("/").pop(),
-        title: gotTitle[0].textContent,
-        local: "/posts/zenn/" + gotId[0].textContent.split("/").pop(),
-        reference: "zenn",
-        createdAt: new Date(gotPubDate[0].textContent),
-      });
-    }
-    return links;
-  };
-
-  let links: LINK[] = [];
-  links.push(...(await getHatenaLinks()));
-  links.push(...(await getZennLinks()));
-  links = links.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
-  links = JSON.parse(JSON.stringify(links));
-  console.dir(links);
-  return {
-    props: {
-      links,
-    },
-    revalidate: 86400,
-  };
-};
 
 export default Blog;
