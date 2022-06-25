dde566ba914d2680be7c9ef3368242101e86c4bb
tamagram <tamadevelopment211@gmail.com>
2022-06-25 16:55:24 +0900

    :sparkles: Update UI

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -8,38 +8,75 @@ const Timeline: NextPage = () => {
     <Layout>
       <Header path="/timeline"></Header>
       <main className={styles.main}>
-        <div className={styles.container}>
-          <div className={styles.container__line}></div>
-          <ul className={styles.container__items}>
-            <li className={styles.container__item}>
-              <div className={styles.container__top}>
-                <div className={styles.container__circle}></div>
-                <div className={styles.container__title}>
-                  Published on 2022-01-07T00:25:56+09:00
+        <div>
+          <div className={styles.year}>2022</div>
+          <div className={styles.container}>
+            <div className={styles.container__line}></div>
+            <ul className={styles.container__items}>
+              <li className={styles.container__item}>
+                <div className={styles.container__top}>
+                  <div className={styles.container__circle}></div>
+                  <div className={styles.container__title}>
+                    Published on 2022-01-07T00:25:56+09:00
+                  </div>
                 </div>
-              </div>
-              <div className={styles.container__desc}>description</div>
-            </li>
+                <div className={styles.container__desc}>description</div>
+              </li>
 
-            <li className={styles.container__item}>
-              <div className={styles.container__top}>
-                <div className={styles.container__circle}></div>
-                <div className={styles.container__title}>
-                  Published on 2022-01-07T00:25:56+09:00
+              <li className={styles.container__item}>
+                <div className={styles.container__top}>
+                  <div className={styles.container__circle}></div>
+                  <div className={styles.container__title}>
+                    Published on 2022-01-07T00:25:56+09:00
+                  </div>
                 </div>
-              </div>
-              <div className={styles.container__desc}>description</div>
-            </li>
-            <li className={styles.container__item}>
-              <div className={styles.container__top}>
-                <div className={styles.container__circle}></div>
-                <div className={styles.container__title}>
-                  Published on 2022-01-07T00:25:56+09:00
+                <div className={styles.container__desc}>description</div>
+              </li>
+              <li className={styles.container__item}>
+                <div className={styles.container__top}>
+                  <div className={styles.container__circle}></div>
+                  <div className={styles.container__title}>
+                    Published on 2022-01-07T00:25:56+09:00
+                  </div>
                 </div>
-              </div>
-              <div className={styles.container__desc}>description</div>
-            </li>
-          </ul>
+                <div className={styles.container__desc}>description</div>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.year}>2021</div>
+          <div className={styles.container}>
+            <div className={styles.container__line}></div>
+            <ul className={styles.container__items}>
+              <li className={styles.container__item}>
+                <div className={styles.container__top}>
+                  <div className={styles.container__circle}></div>
+                  <div className={styles.container__title}>
+                    Published on 2022-01-07T00:25:56+09:00
+                  </div>
+                </div>
+                <div className={styles.container__desc}>description</div>
+              </li>
+
+              <li className={styles.container__item}>
+                <div className={styles.container__top}>
+                  <div className={styles.container__circle}></div>
+                  <div className={styles.container__title}>
+                    Published on 2022-01-07T00:25:56+09:00
+                  </div>
+                </div>
+                <div className={styles.container__desc}>description</div>
+              </li>
+              <li className={styles.container__item}>
+                <div className={styles.container__top}>
+                  <div className={styles.container__circle}></div>
+                  <div className={styles.container__title}>
+                    Published on 2022-01-07T00:25:56+09:00
+                  </div>
+                </div>
+                <div className={styles.container__desc}>description</div>
+              </li>
+            </ul>
+          </div>
         </div>
       </main>
       <Footer />
